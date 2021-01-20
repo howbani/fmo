34a8fde88605fd146bb7b049bc2d28b56c0990bb
Ammar Hawbani <33313489+howbani@users.noreply.github.com>
2021-01-20 11:16:37 +0800

    Add files via upload

---
+++ ui/UIExpermentResults.xaml.cs
@@ -0,0 +1,24 @@
+﻿using FMO.Charts;
+using FMO.Intilization;
+using FMO.ExpermentsResults;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace FMO.ui
+{
+    /// <summary>
+    /// Interaction logic for ExpermentResults.xaml
+    /// </summary>
+    public partial class UIExpermentResults : Window 
+    {
+        public UIExpermentResults() 
+        {
+            InitializeComponent();
+            /*
+            List<CompareResult> list = new List<CompareResult>();
+            CompareResult res = CompareResultsClass.GetEnergyDelayForAnExperment();
+            list.Add(res);
+            dg_data.ItemsSource = list;*/
+        }
+    }
+}
